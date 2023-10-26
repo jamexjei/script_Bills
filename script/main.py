f37ffb218c7af9094805c91746cf1a5b6b34e84d
jamexjei <jamexjei@gmail.com>
2023-10-26 15:00:53 -0500

    added new validation

--- script/main.py
+++ script/main.py
@@ -1,26 +1,5 @@
 import subprocess
 import time
-comando = "pip install PyMuPDF"
-comando1 = "pip install pyzbar"
-comando5="pip install Pillow"
-comando6="pip install PyMuPDF qrcode opencv-python"
-try:
-    subprocess.call(comando, shell=True)
-    print("installing PyMuPDF")
-    subprocess.call(comando1, shell=True)
-    print("installing pyzbar")
-    subprocess.call(comando5,shell=True)
-    print("installing  Pillow")
-    subprocess.call(comando6,shell=True)
-    print("installing qr code ")
-    time.sleep(1)
-    
-    print("Dependencies installed correctly.")
-except subprocess.CalledProcessError as e:
-    print("Error  installing all dependencies:", e)
-    
-  
-
 from random import randint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -28,10 +7,13 @@ from selenium.webdriver.common.keys import Keys
 import os
 from pdf2image import convert_from_path
 import cv2
+import aspose.barcode as barcode
+import json
+import re
 poppler_path = "../poppler/bin"
 pdf_folder = "../invoice_upload"
 image_folder = "../invoice_images"
-
+list_facturas=[]
 def generate_pdf_to_image():
     os.makedirs(image_folder, exist_ok=True)
 
@@ -57,26 +39,98 @@ def find_and_decode_qr_codes():
         #img = Image.open(img_path)
         img = cv2.imread(img_path)
         det = cv2.QRCodeDetector()
-        x, y = 1, 1
+        
 
     for img_file in os.listdir(image_folder):
+        print(f"procesando--> {img_file} ")
+        print(f"decodificando QR... {img_file}")
+        
+        x, y = 3, 3
+        sw=0
         img_path = os.path.join(image_folder, img_file)
         img = cv2.imread(img_path)
-
-        while x < 20 and y < 20:
+        percentage=10
+        loading=f" loading {percentage}%"
+        data={}
+        itera=''
+        while x < 7 and y < 7:
+            
             scaled_image = cv2.resize(img, None, fx=x, fy=y, interpolation=cv2.INTER_CUBIC)
             retval, decoded_info, points, straight_qrcode = det.detectAndDecodeMulti(scaled_image)
+            
+            if len(decoded_info) > 0 and decoded_info[0]!='':
+                itera=decoded_info[0]
+                sw=1    
+                print(f"Código QR encontrado en {img_file} ")
+                print("----------------------------")
+                lineas = itera.strip().split('\n')
 
-            if len(decoded_info) > 0 and decoded_info!="'',":
-                print(f"Código QR encontrado en {img_file} a escala {x} x {y}")
-                print(decoded_info)
+
+                data = {}
+
+
+                for linea in lineas:
+                    if ':' in linea:
+                        clave, valor = re.split(r':', linea, maxsplit=1)
+                        data[clave] = valor
+                    else:
+        
+                        data["URL"] = linea
+
+
+                json_data = json.dumps(data, indent=4)
+                list_facturas.append(json_data)
+
                 x=1
                 y=1
                 break
 
-            x += 2
-            y += 2
-            print(f"Escala actual: {x} x {y}")
+            x += 3
+            y += 3
+            
+            
+            
+        if sw==0:
+            print("usando metodo 2 de deteccion")
+            data, bbox, straight_qrcode = det.detectAndDecode(img)
+            if len(data)>0 and data[0]!='':
+                print(f"Código QR encontrado en {img_file} ")
+                print("----------------------------")
+                print(data)
+            else:
+                print("usando metodo 3 de deteccion")
+                reader = barcode.barcoderecognition.BarCodeReader(img_path)
+                reader.quality_settings.read_tiny_barcodes = True
+                reader.quality_settings.allow_incorrect_barcodes = True
+                reader.quality_settings.allow_qr_micro_qr_restoration = True
+                recognized_results = reader.read_bar_codes()
+                if len (recognized_results)>0:
+                    print(f"Código QR encontrado en {img_file} ")
+                    print("----------------------------")
+                    for x in recognized_results:
+                        if x.code_text !='':
+                            lineas = x.code_text.strip().split('\n')
+
+                            data = {}
+
+
+                            for linea in lineas:
+                                if ':' in linea:
+                                    clave, valor = re.split(r':', linea, maxsplit=1)
+                                    data[clave] = valor
+      
+                                    data["URL"] = linea
+
+
+                            json_data = json.dumps(data, indent=4)
+
+                            list_facturas.append(json_data)
+                            
+                        break
+                else:
+                    print(f"no se encontro QR para {img_file}")
+                    print("----------------------------")
+            
     for img_file in os.listdir(image_folder):
         img_path = os.path.join(image_folder, img_file)
         os.remove(img_path)
@@ -87,3 +141,9 @@ def find_and_decode_qr_codes():
 
 generate_pdf_to_image()
 find_and_decode_qr_codes()
+data={}
+list_2=[]
+print("los resultados obtenidos son : ")
+print("-------------------------------------")
+print(list_facturas)
+
